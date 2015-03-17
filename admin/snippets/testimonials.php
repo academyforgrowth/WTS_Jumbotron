7df85497dff6ac06bba67c91f6cfdb88b6f48125
Afton Rhodes <aftonrhodes@gmail.com>
2015-03-17 10:45:54 +0000

    new admin snippet

---
+++ admin/snippets/testimonials.php
@@ -0,0 +1,61 @@
+
+
+            <div class="panel panel-default testimonial">
+                <div class="panel-body">
+                    <blockquote class="testimonial">
+                        <h4>Headline</h4>
+                        <p>Testimonial</p>
+                        <p class="text-right"><small>Client Name</small></p>
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="panel panel-default testimonial">
+                <div class="panel-body">
+                    <blockquote class="testimonial">
+                        <h4>Headline</h4>
+                        <p>Testimonial</p>
+                        <p class="text-right"><small>Client Name</small></p>
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="panel panel-default testimonial">
+                <div class="panel-body">
+                    <blockquote class="testimonial">
+                        <h4>Headline</h4>
+                        <p>Testimonial</p>
+                        <p class="text-right"><small>Client Name</small></p>
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="panel panel-default testimonial">
+                <div class="panel-body">
+                    <blockquote class="testimonial">
+                        <h4>Headline</h4>
+                        <p>Testimonial</p>
+                        <p class="text-right"><small>Client Name</small></p>
+                    </blockquote>
+                </div>
+            </div>
+            
+            <div class="panel panel-default testimonial">
+                <div class="panel-body">
+                    <blockquote class="testimonial">
+                        <h4>Headline</h4>
+                        <p>Testimonial</p>
+                        <p class="text-right"><small>Client Name</small></p>
+                    </blockquote>
+                </div>
+            </div>
+            
+            <div class="panel panel-default testimonial">
+                <div class="panel-body">
+                    <blockquote class="testimonial">
+                        <h4>Headline</h4>
+                        <p>Testimonial</p>
+                        <p class="text-right"><small>Client Name</small></p>
+                    </blockquote>
+                </div>
+            </div>
