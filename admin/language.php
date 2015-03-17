9f6ee6b3c6b6b937aa174417d47f2c6461fb7a32
Afton Rhodes <aftonrhodes@gmail.com>
2015-03-17 10:23:33 +0000

    new variable snippet files added

---
+++ admin/language.php
@@ -0,0 +1,44 @@
+<?php  
+
+// ********************************************************** //
+//
+// TABLE OF CONTENTS
+//
+// 1. =ENGLISH, UNITED KINGDOM
+// 2. =ENGLISH, UNITED STATES.
+//
+// ********************************************************** //
+// 
+// ~~~~~~~~~~~~~~~~~~~~~~~INSTRUCTIONS~~~~~~~~~~~~~~~~~~~~~~~ //
+// 
+// Locate the correct language for the site and 'uncomment' it.
+// Make sure that you also 'comment out' the default language.
+// 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// 
+
+
+/******************************
+ ** =ENGLISH, UNITED KINGDOM
+ ******************************/
+
+$favourite = 'favourite';
+
+$recognise = 'recognise';
+
+$maximise = 'maximise';
+
+
+/******************************
+ ** =ENGLISH, UNITED STATES
+ ******************************/
+
+// $favourite = 'favorite';
+
+// $recognise = 'recognize';
+
+// $maximise = 'maximize';
+
+
+?>
+
---
+++ admin/snippets/team.php
@@ -0,0 +1,124 @@
+<!-- LEAD SECTION -->
+    <section id ="lead">
+        <div class="container">
+            
+            <div class="row">
+                
+                <div class="col-12 col-lg-12 text-center">
+                    <div class="page-header">
+                        <h1>Join Our Team</h1>
+                        <h2> We take a slightly different view to most other firms when it comes to the concept of "the team".</span></h2>
+                        <p class="lead">We firmly believe that <strong><span class="text-info">The Team is what happens when we work together</span> - US <U>AND</U> YOU.</strong> </p>
+                        <p class="lead">We all prosper when we're successful together, that's why we work so hard to build a solid relationship with you and all our other clients. Its the <strong>TEAM ETHOS</strong> that sets us apart from other firms.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- END LEAD SECTION -->
+
+
+    <!-- TEAM SECTION -->
+    <section class="altcolor2" id="team">
+        <div class="container">
+            
+            <div class="row team-row">
+
+                <div class="col-sm-offset-1 col-sm-3">
+                    <img alt="" class="img-circle img-responsive team-img" src="images/avafour.jpg" />
+                </div>
+                <div class="col-sm-offset-1 col-sm-7">
+                    <h3>John Smith</h3>
+                    <p>Job Title</p>
+                    <p>Bio</p>
+                </div>
+            
+            </div>
+            
+            <hr />
+
+            <div class="row team-row">
+            
+                <div class="col-sm-offset-1 col-sm-3">
+                    <img alt="" class="img-circle img-responsive team-img" src="images/avafour.jpg" />
+                </div>
+                <div class="col-sm-offset-1 col-sm-7">
+                    <h3>John Smith</h3>
+                    <p>Job Title</p>
+                    <p>Bio</p>
+                </div>
+            
+            </div>
+
+            <hr />
+
+            <div class="row team-row">
+
+                <div class=" col-sm-offset-1 col-sm-3">
+                    <img alt="" class="img-circle img-responsive team-img" src="images/avafour.jpg" />
+                </div>
+                <div class="col-sm-offset-1 col-sm-7">
+                    <h3>John Smith</h3>
+                    <p>Job Title</p>
+                    <p>Bio</p>
+                </div>
+            
+            </div>
+
+            <hr />
+
+            <div class="row team-row">
+
+                <div class="col-sm-offset-1 col-sm-3">
+                    <img alt="" class="img-circle img-responsive team-img" src="images/avafour.jpg" />
+                </div>
+                <div class="col-sm-offset-1 col-sm-7">
+                    <h3>John Smith</h3>
+                    <p>Job Title</p>
+                    <p>Bio</p>
+                </div>
+            
+            </div>
+
+            <hr />
+
+            <div class="row team-row">
+
+                <div class=" col-sm-offset-1 col-sm-3">
+                    <img alt="" class="img-circle img-responsive team-img" src="images/avafour.jpg" />
+                </div>
+                <div class="col-sm-offset-1 col-sm-7">
+                    <h3>John Smith</h3>
+                    <p>Job Title</p>
+                    <p>Bio</p>
+                </div>
+            
+            </div>
+
+            <hr />
+
+            <div class="row team-row">
+
+                <div class="col-sm-offset-1 col-sm-3">
+                    <img alt="" class="img-circle img-responsive team-img" src="images/avafour.jpg" />
+                </div>
+                <div class="col-sm-offset-1 col-sm-7">
+                    <h3>John Smith</h3>
+                    <p>Job Title</p>
+                    <p>Bio</p>
+                </div>
+
+            </div>
+
+            <div class="row">
+
+                <p class="lead text-center">So if you have an urgent question or just a quick query, everyone here is ready and waiting to help you...</p>
+                <p class="lead text-center conclusion">&hellip;and that includes the partners of the firm, who you have direct access to whenever you need it.</p>
+            
+            </div>
+
+        </div>
+    </section>
+    <!-- END FEATURE -->
