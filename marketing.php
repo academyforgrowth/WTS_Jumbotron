da1e1d298a5d8faac3537cfd99aeaa1f9d749a05
Afton Rhodes <aftonrhodes@gmail.com>
2015-03-17 10:46:11 +0000

    Updated snippets

--- marketing.php
+++ marketing.php
@@ -88,71 +88,7 @@ include 'admin/var.php';
     <!-- CALL TO ACTION END -->
 
     <!-- TESTIMONIAL HEADLINES -->
-    <section id="testimonial">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 page-header page-header-custom text-center">
-                    <h2 class="headline">HERE'S WHAT OUR CLIENTS HAVE BEEN SAYING ABOUT OUR UNIQUE APPROACH TO ACCOUNTANCY</h2>
-                </div>
-                
-                <div class="row">
-                    
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <blockquote>
-                                    <p>Headline</p>
-                                    <small>Client Name</small>
-                                </blockquote>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <blockquote>
-                                    <p>Headline</p>
-                                    <small>Client Name</small>
-                                </blockquote>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <blockquote>
-                                    <p>Headline</p>
-                                    <small>Client Name</small>
-                                </blockquote>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <blockquote>
-                                    <p>Headline</p>
-                                    <small>Client Name</small>
-                                </blockquote>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="clearfix"></div>
-
-                <div class="text-center">
-                    <a href="testimonials.php" class="btn btn-success btn-md more">More Comments from Clients</a>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php include ("admin/snippets/top_testimonials.php"); ?>
     <!-- TESTIMONIAL HEADLINES END -->
 
     <!-- MEETING -->
--- testimonials.php
+++ testimonials.php
@@ -90,67 +90,9 @@ include 'admin/var.php';
             
             </div>
 
-            <!-- TESTIMONIAL =============================== -->
-
-            <div class="panel panel-default testimonial">
-                <div class="panel-body">
-                    <blockquote class="testimonial">
-                        <h4>Headline</h4>
-                        <p>Testimonial</p>
-                        <p class="text-right"><small>Client Name</small></p>
-                    </blockquote>
-                </div>
-            </div>
-
-            <div class="panel panel-default testimonial">
-                <div class="panel-body">
-                    <blockquote class="testimonial">
-                        <h4>Headline</h4>
-                        <p>Testimonial</p>
-                        <p class="text-right"><small>Client Name</small></p>
-                    </blockquote>
-                </div>
-            </div>
-
-            <div class="panel panel-default testimonial">
-                <div class="panel-body">
-                    <blockquote class="testimonial">
-                        <h4>Headline</h4>
-                        <p>Testimonial</p>
-                        <p class="text-right"><small>Client Name</small></p>
-                    </blockquote>
-                </div>
-            </div>
-
-            <div class="panel panel-default testimonial">
-                <div class="panel-body">
-                    <blockquote class="testimonial">
-                        <h4>Headline</h4>
-                        <p>Testimonial</p>
-                        <p class="text-right"><small>Client Name</small></p>
-                    </blockquote>
-                </div>
-            </div>
-            
-            <div class="panel panel-default testimonial">
-                <div class="panel-body">
-                    <blockquote class="testimonial">
-                        <h4>Headline</h4>
-                        <p>Testimonial</p>
-                        <p class="text-right"><small>Client Name</small></p>
-                    </blockquote>
-                </div>
-            </div>
-            
-            <div class="panel panel-default testimonial">
-                <div class="panel-body">
-                    <blockquote class="testimonial">
-                        <h4>Headline</h4>
-                        <p>Testimonial</p>
-                        <p class="text-right"><small>Client Name</small></p>
-                    </blockquote>
-                </div>
-            </div>
+            <!-- TESTIMONIALS START -->
+            <?php include ("admin/snippets/testimonials.php"); ?>
+            <!-- TESTIMONIALS END -->
 
 	   </div>
     </section>
