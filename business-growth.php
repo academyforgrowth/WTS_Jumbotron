1f526e8d1acae9a63dec561e7222124c8135afe7
Afton Rhodes <aftonrhodes@gmail.com>
2015-03-17 10:32:23 +0000

    PHP include for assoc logo sections

--- business-growth.php
+++ business-growth.php
@@ -288,47 +288,7 @@ include 'admin/var.php';
     </section>
 
     <!-- ASSOCIATIONS -->
-    
-    <div style="margin-bottom: 0px; margin-top: 0px;" id="associations" class="well">
-        <div class="container-fluid">
-            
-            <div class="row">
-                
-                <div class="col-xs-6 col-sm-4 col-md-offset-2 col-md-2 text-center">
-                    <div class="well-sm">
-                        <img alt="" width="100" height="100" src="images/assoclogo1.png" />
-                    </div>
-                </div>
-
-                <div class="col-xs-6 col-sm-4 col-md-2 col-lg-2 text-center">
-                    <div class="well-sm">
-                        <img alt="" width="100" height="100" src="images/assoclogo2.png" />
-                    </div>
-                </div>
-
-                <div class="col-xs-6 col-sm-4 col-md-2 col-lg-2 text-center">
-                    <div class="well-sm">
-                        <img alt="" width="100" height="100" src="images/assoclogo3.png" />
-                    </div>
-                </div>
-
-                <div class="col-xs-6 col-sm-4 col-md-2 col-lg-2 text-center">
-                    <div class="well-sm">
-                        <img alt="" width="100" height="100" src="images/assoclogo4.png" />
-                    </div>
-                </div>
-
-                <div class="col-xs-6 col-sm-4 col-md-2 col-lg-2 text-center">
-                    <div class="well-sm">
-                        <img alt="" width="100" height="100" src="images/assoclogo5.png" />
-                    </div>
-                </div>
-            
-            </div>
-        
-        </div>
-    </div>
-
+    <?php include ("admin/snippets/assoc_logos.php"); ?>
     <!-- END ASSOCIATIONS -->
 
     <!-- FOOTER START -->
